478a924ed1129edf1afc85a1d0dced539b1062c6
Matthew Harrigan <harrigan@stanford.edu>
2014-09-22 17:53:06 -0700

    Docs

--- mattools/pickleutils.py
+++ mattools/pickleutils.py
@@ -4,5 +4,9 @@ __all__ = ['pload']
 
 
 def pload(fn):
+    """Load from a filename real quick
+
+    Side note: why doesn't pickle define this function for me??
+    """
     with open(fn, 'rb') as f:
         return pickle.load(f)
